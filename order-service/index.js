90c8610e853ee3f1bdf34ce67cd1ff6641771530
omanthi <omanthibandara326@gmail.com>
2026-04-28 01:07:46 +0530

    Add debug part

--- order-service/index.js
+++ order-service/index.js
@@ -23,6 +23,14 @@ connectRabbitMQ();
 app.use("/order", orderRoute);
 app.get('/metrics', metricsEndpoint);
 
+app.get("/debug-jwt", (req, res) => {
+  res.json({
+    jwt_secret_set: !!process.env.JWT_SECRET,
+    jwt_secret_length: process.env.JWT_SECRET?.length,
+    jwt_secret_value: process.env.JWT_SECRET
+  });
+});
+
 app.listen(process.env.PORT, () =>
   console.log(`🚀 Order Service running on port ${process.env.PORT}`)
 );
